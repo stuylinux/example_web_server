943c8d77a6e29e6420495d6fff595e7e80a95c91
leomet07 <leomet07@gmail.com>
2023-02-17 14:40:27 -0500

    Use morgan as a logger and return OS info on request

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+const express = require("express");
+const morgan = require("morgan");
+const fs = require("fs");
+const app = express();
+const port = 3000;
 
-const express = require('express')
-const app = express()
-const port = 3000
+let os_file = fs.readFileSync("/etc/os-release", "utf8");
+const os_info = os_file.split("\n").splice(0, 3);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(morgan("tiny"));
+
+app.get("/", (req, res) => {
+	res.send("Hello World from " + String(os_info));
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+	console.log(`Example app listening on port ${port}`);
+});
